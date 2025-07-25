f55b1a7e1ea6afc199895f697b0b36977a18e0df
JosueHenriquez <josueguinea@gmail.com>
2025-07-24 22:16:29 -0600

    Primer commit

---
+++ src/main/java/IntegracionBackFront/backfront/BackfrontApplication.java
@@ -0,0 +1,13 @@
+package IntegracionBackFront.backfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackfrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackfrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/IntegracionBackFront/backfront/BackfrontApplicationTests.java
@@ -0,0 +1,13 @@
+package IntegracionBackFront.backfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackfrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
